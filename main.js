51da585c1de46ba5b9f149f1e14b383e72b936b7
Joshua Shunk <aerixia@gmail.com>
2023-12-14 11:01:59 -0500

    Add in factory function for player creation, and a basic gameboard

--- main.js
+++ main.js
@@ -1,1 +1,25 @@
 console.log("Hello");
+
+// () => {
+// 	gameBoard = { board: [1, 1, 1, 1, 1, 1, 1, 1, 1] };
+// 	return gameBoard;
+// };
+
+// let gameBoard = { board: [1, 1, 1, 1, 1, 1, 1, 1, 1] };
+
+const gameBoard = (function () {
+	return { board: [1, 1, 1, 1, 1, 1, 1, 1, 1] };
+})();
+
+function createPlayer(playerNumber, playerToken) {
+	return {
+		playerNumber: playerNumber,
+		playerToken: playerToken,
+		makePlay(x) {
+			gameBoard.board[x] = playerToken;
+		},
+	};
+}
+
+const player1 = createPlayer(1, "X");
+const player2 = createPlayer(2, "O");
