a47194646eadf8717e32ef0f2bc62ac8adfdbf43
Joshua Shunk <aerixia@gmail.com>
2023-12-21 16:45:10 -0500

    Add in basic turn structure. Need to add logic for turn order.

--- main.js
+++ main.js
@@ -11,12 +11,33 @@ const gameBoard = (function () {
 	return { board: [1, 1, 1, 1, 1, 1, 1, 1, 1] };
 })();
 
+const gameLogic = function () {
+	return {};
+};
+
 function createPlayer(playerNumber, playerToken) {
+	const playerTurn = true;
 	return {
 		playerNumber: playerNumber,
 		playerToken: playerToken,
+		playerTurn,
 		makePlay(x) {
-			gameBoard.board[x] = playerToken;
+			if (this.playerTurn === true) {
+				gameBoard.board[x] = playerToken;
+				console.log(`
+					${gameBoard.board.slice(0, 3)}\n
+					${gameBoard.board.slice(3, 6)}\n
+					${gameBoard.board.slice(6, 9)}`);
+				console.log(this.playerNumber);
+				this.playerTurn = false;
+				if (this.playerTurn === 1) {
+					player2.playerTurn = true;
+				} else if (this.playerTurn === 2) {
+					player1.playerTurn = true;
+				}
+			} else {
+				console.log(`It is not player ${this.playerNumber}'s turn to play.`);
+			}
 		},
 	};
 }
